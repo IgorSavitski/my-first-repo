5b6591334d4c3f293eb21894775062857c9d7a49
Igor Savitski <igorsavickij47@gmail.com>
2025-11-09 14:03:07 +0300

    New playwright tests

---
+++ tests/failed_login.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Авторизация с ошибкой на Sauce Demo', () => {
+
+  test('Пользователь должен получить ошибку при входе в систему', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+  });
+});
